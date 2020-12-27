303654e2ac2226fa629d87ff7e9d6340f61e0366
Silvio <molinahuel44@gmail.com>
2020-12-27 00:57:30 -0300

    attempt adding the libraries like jacob and jna, i am not able to build something productive with this tools, but at least i already have what i need to start derouler something

--- src/Container/JacobClass.java
+++ src/Container/JacobClass.java
@@ -2,16 +2,8 @@ package Container;
 
 import com.mysql.jdbc.Buffer;
 
-import com.oracle.net.Sdp;
-import com.oracle.webservices.internal.api.message.MessageContextFactory;
-import com.oracle.webservices.internal.api.message.ReadOnlyPropertyException;
-
 
 public class JacobClass {
     public static void main(){
-        System.out.println(ReadOnlyPropertyException.class.getClass());
-        System.out.println(MessageContextFactory.class.getClass());
-        System.out.println(Sdp.class.getClass());
-        System.out.println(Buffer.class.getClass());
     }
 }
--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -5,14 +5,13 @@ import Container.JacobClass;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
+
 import com.sun.jna.win32.DLLCallback;
-import com.sun.jna.win32.W32APIFunctionMapper;
-import com.sun.jna.win32.W32APIOptions;
 import com.sun.jna.win32.StdCallLibrary;
 
 public interface JavaApplication2 extends StdCallLibrary {
         
     JavaApplication2 INSTANCE = (JavaApplication2) Native.load("kernel32",JavaApplication2.class);
     
-    JavaApplication2 SYNC_INSTANCE = (JavaApplication2) Native.syncronizedLibrary(INSTANCE);
+    JavaApplication2 SYNC_INSTANCE = (JavaApplication2) Native.synchronizedLibrary(INSTANCE);
 }
