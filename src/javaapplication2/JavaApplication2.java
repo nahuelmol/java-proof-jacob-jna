2005f4bb350eee52ae6a520dc52f9e4fcc17e6a4
Silvio Molina <molinahuel44@gmail.com>
2021-04-15 01:22:26 -0300

    interface added but it need to be customize, i have to think on integrate more functions, being more complete

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -14,19 +14,13 @@ import java.lang.Object;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-//import com.sun.jna.Library;
-//import com.sun.jna.Native;
-//import com.sun.jna.Platform;
 
-//import com.sun.jna.win32.DLLCallback;
-//import com.sun.jna.win32.StdCallLibrary;
 
 public class JavaApplication2 {
     public static void main(String[] args){
         File file = new File("D:\\javaprove\\new_culoabierto.txt");
         
         Path path = FileSystems.getDefault().getPath("D:\\javaprove\\main.txt");
-        Path Tempath = FileSystems.getDefault().getPath("C:\\Users\\Alumno\\AppData\\Local\\Temp");
         
         boolean result;
         try {
@@ -50,8 +44,4 @@ public class JavaApplication2 {
 }
 
 
-public class removingTemp {
-    public static void main(String[] args){
-        
-    }
-}
+
